0e4006aa94a0634b336aa2581f8be983ac36011a
Your Name <you@example.com>
2011-10-10 17:20:34 +0200

    Corrected color transfer of fill-color, optimized syntax.

--- vigranumpy/lib/pyqt/viewer2svg.py
+++ vigranumpy/lib/pyqt/viewer2svg.py
@@ -1,8 +1,8 @@
-import os 
+import os
 from PyQt4 import QtCore, QtGui
 
-def viewer2svg(viewer, basepath, onlyVisible = False, moveBy = QtCore.QPointF(0.5, 0.5)):
-    outvec=[]
+def viewer2svg(viewer, basepath, onlyVisible=False, moveBy=QtCore.QPointF(0.5, 0.5)):
+    outvec = []
     outvec.append('<?xml version="1.0" standalone="no"?>\n')
     outvec.append('<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"\n')
     outvec.append('  "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n')
@@ -19,7 +19,7 @@ def viewer2svg(viewer, basepath, onlyVisible = False, moveBy = QtCore.QPointF(0.
 
     outvec.append('\n<image xlink:href="' + bgFilename + '"\n')
     outvec.append('  x="0" y="0" width="' + str(viewer.image.width) + '" height="' + \
-      str(viewer.image.height) + '" />\n')
+      str(viewer.image.height) + '" />\n\n')
 
     ovs = []
     for ov in viewer.overlays:
@@ -32,24 +32,30 @@ def viewer2svg(viewer, basepath, onlyVisible = False, moveBy = QtCore.QPointF(0.
         elif ovname == "OverlayGroup":
             for subov in ov.overlays:
                 ovname = viewer._defaultOverlayName(subov)
-                ovs.append([ovname, subov])            
+                ovs.append([ovname, subov])
         else:
-            ovs.append([ovname, ov])            
+            ovs.append([ovname, ov])
 
     for overlay in ovs:
         if overlay[0] == "EdgeOverlay":
             overlay = overlay[1]
-            color = 'rgb' + str(overlay.color.getRgb()[:3]) + '; opacity:' + str(overlay.color.getRgb()[-1] / 255.0)
+            color = 'rgb' + str(overlay.color.getRgb()[:3]) + '; stroke-opacity:' \
+              + str(overlay.color.getRgb()[-1] / 255.0)
+            fillColor = overlay.fillColor
+            if fillColor:
+                fillColor = 'fill:rgb' + str(fillColor.getRgb()[:3]) + '; fill-opacity:' \
+                  + str(fillColor.getRgb()[-1] / 255.0)
             if not overlay.colors:
                 for i, edge in enumerate(overlay.originalEdges):
-                    outvec.append(writeEdge(edge, overlay.width, color, moveBy))
+                    outvec.append(writeEdge(edge, overlay.width, color, fillColor, moveBy))
             else:
                 for i, edge in enumerate(overlay.originalEdges):
                     if len(overlay.colors) > i:
                         color = overlay.colors[i] if hasattr(overlay.colors[i], "getRgb") else \
                           QtGui.QColor(overlay.colors[i])
-                        color = 'rgb' + str(color.getRgb()[:3]) + '; opacity:' + str(color.getRgb()[-1] / 255.0)
-                    outvec.append(writeEdge(edge, overlay.width, color, moveBy))
+                        color = 'rgb' + str(color.getRgb()[:3]) + '; opacity:' \
+                          + str(color.getRgb()[-1] / 255.0)
+                    outvec.append(writeEdge(edge, overlay.width, color, fillColor, moveBy))
         elif overlay[0] == "PointOverlay":
             overlay = overlay[1]
             pointList = []
@@ -59,31 +65,37 @@ def viewer2svg(viewer, basepath, onlyVisible = False, moveBy = QtCore.QPointF(0.
             if overlay.colors:
                 for i, point in enumerate(pointList):
                     if len(overlay.colors) > i:
-                        color = '  style="stroke:rgb' + str(overlay.colors[i].getRgb()[:3]) + '; stroke-opacity:' \
-                          + str(overlay.colors[i].getRgb()[-1] / 255.0) + ';\n'
-                        fillcolor = '  fill:rgb' + str(overlay.colors[i].getRgb()[:3]) + '; fill-opacity:' \
-                          + str(overlay.colors[i].getRgb()[-1] / 255.0) + '"/>\n'
-                    outvec.append('<circle cx="' + str(point.x()) + '" cy="' + str(point.y()) + radius + color + fillcolor)
+                        ovColor = overlay.colors[i].getRgb()
+                        color = '  style="stroke:rgb' + str(ovColor[:3]) + \
+                          '; stroke-opacity:' + str(ovColor[-1] / 255.0) \
+                          + '; stroke-width:0.3;\n'
+                        fillColor = '  fill:rgb' + str(ovColor[:3]) + \
+                          '; fill-opacity:' + str(ovColor[-1] / 255.0) + '"/>\n\n'
+                    outvec.append('<circle cx="' + str(point.x()) + '" cy="' + \
+                      str(point.y()) + radius + color + fillColor)
             else:
-                color = '  style="stroke:rgb' + str(overlay.color.getRgb()[:3]) + '; stroke-opacity:' \
-                  + str(overlay.color.getRgb()[-1] / 255.0)
+                color = '  style="stroke:rgb' + str(overlay.color.getRgb()[:3]) \
+                  + '; stroke-opacity:' + str(overlay.color.getRgb()[-1] / 255.0) \
+                  + '; stroke-width:0.3'
                 if overlay.fillColor:
-                    fillcolor = ';\n  fill:rgb' + str(overlay.fillColor.getRgb()[:3]) + '; fill-opacity:' \
-                      + str(overlay.fillColor.getRgb()[-1] / 255.0) + '"/>\n'
+                    fillColor = ';\n  fill:rgb' + str(overlay.fillColor.getRgb()[:3]) + \
+                      '; fill-opacity:' + str(overlay.fillColor.getRgb()[-1] / 255.0) + '"/>\n\n'
                 else:
-                    fillcolor = '"/>\n'
+                    fillColor = '"/>\n\n'
                 for point in pointList:
-                    outvec.append('<circle cx="' + str(point.x()) + '" cy="' + str(point.y()) + radius + color + fillcolor)
+                    outvec.append('<circle cx="' + str(point.x()) + '" cy="' + str(point.y())
+                      + radius + color + fillColor)
         elif overlay[0] == "TextOverlay":
             overlay = overlay[1]
             if overlay.pointsize:
-                outvec.extend(writeText(overlay.text, overlay.pos, overlay.color, overlay.pointsize))
+                outvec.extend(writeText(overlay.text, overlay.pos, \
+                  overlay.color, overlay.pointsize))
             else:
                 outvec.extend(writeText(overlay.text, overlay.pos, overlay.color))
         else:
             print str(overlay[0]) + " not supported yet.\n"
 
-    outvec.append('\n</g>\n')
+    outvec.append('</g>\n')
     outvec.append('</svg>\n')
 
     f = open(basepath + ".svg", 'w')
@@ -91,11 +103,11 @@ def viewer2svg(viewer, basepath, onlyVisible = False, moveBy = QtCore.QPointF(0.
         f.write(line)
     f.close()
 
-def writeEdge(edge, width, color, moveBy):
+def writeEdge(edge, width, color, fillColor, moveBy):
     qpolf = QtGui.QPolygonF(len(edge))
     for i, (x, y) in enumerate(edge):
-        qpolf[i] = QtCore.QPointF(x,y) + moveBy
-    result = "\n"
+        qpolf[i] = QtCore.QPointF(x, y) + moveBy
+    result = ""
     if qpolf.size() == 2:
         result += '<line x1="' + str(qpolf[0].x()) + '" y1="' + str(qpolf[0].y()) + '" '
         result += 'x2="' + str(qpolf[1].x()) + '" y2="' + str(qpolf[1].y())
@@ -103,23 +115,27 @@ def writeEdge(edge, width, color, moveBy):
         result += '<polyline points="' + str(qpolf[0].x()) + ' ' + str(qpolf[0].y())
         for pos in range(1, qpolf.size()):
             result += ' ' + str(qpolf[pos].x()) + ' ' + str(qpolf[pos].y())
-    result += '"\n  style="stroke:' + color + '; stroke-width:' + str(width if width > 0 else 0.5) + ';\n'
-    result += '  stroke-linejoin:bevel; stroke-linecap:butt;"/>\n'
+    result += '"\n  style="stroke:' + color
+    result += '; stroke-width:' + str(width if width > 0 else 0.5) + ';\n'
+    result += '  stroke-linejoin:bevel; stroke-linecap:butt;'
+    if fillColor and qpolf.isClosed():
+        result += '\n  ' + fillColor
+    result += '"/>\n\n'
     return result
 
-def writeText(text, position, color = None, size = None):
+def writeText(text, position, color=None, size=None):
     if not size:
-        size= "6"
+        size = "6"
     if not color:
         color = 'fill:rgb(0, 255, 0); opacity:1; stroke:rgb(0, 0, 0); stroke-width:0.3;'
     else:
-        color = 'fill:rgb' + str(QtGui.QColor(color).getRgb()[:3]) + '; opacity:' + \
-          str(QtGui.QColor(color).getRgb()[-1] / 255.0) + '; stroke:rgb(0, 0, 0); stroke-width:0.3;'
+        color = 'fill:rgb' + str(QtGui.QColor(color).getRgb()[:3]) + \
+          '; opacity:' + str(QtGui.QColor(color).getRgb()[-1] / 255.0) + \
+          '; stroke:rgb(0, 0, 0); stroke-width:0.3;'
     style = '  style="' + color + '\n    dominant-baseline: central; ' + \
       'text-anchor: middle; font-size: ' + str(size) + 'pt; font-family: sans-serif"'
     if text.__class__.__name__ == "QString":
         text = text.toUtf8().data()
 
-    return '\n<text x="' + str(position[0]) + '" y="' + str(position[1]) + '"\n' + \
-            style + '>' + text + '</text>\n'
-
+    return '<text x="' + str(position[0]) + '" y="' + str(position[1]) + '"\n' + \
+            style + '>' + text + '</text>\n\n'
